5815cb31b971714815aed3a13715b747289b6eb4
MayaraOliveir <mayaraoliveir00@gmail.com>
2025-08-27 12:12:08 -0300

    alterando limite para para 100

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -13,7 +13,7 @@ function exibirTexto(tag, texto) {
 function exibirMensagemInicial() {
 
     exibirTexto('h1', 'Jogo do Número Secreto');
-    exibirTexto('p', 'Escolha um número entre 1 a 10');
+    exibirTexto('p', 'Escolha um número entre 1 a 100');
 
 }
 
